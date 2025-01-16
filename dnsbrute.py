ec307ca7de324f2c5555bb5c00ff4e4ce0ec92d6
Larissa Raimee Gomes <srta.raimee@gmail.com>
2025-01-16 12:21:08 -0300

    update

--- dnsbrute.py
+++ dnsbrute.py
@@ -1,16 +1,23 @@
+
 import dns.resolver
 
 res = dns.resolver.Resolver()
-arquivo = open("wordlist.txt", "r")
-subdominios = arquivo.read().splitlines()
 
+with open("wordlist.txt", "r") as arquivo:
+    # just to ignore blank spaces in the wordlist file
+    subdominios = [linha.strip() for linha in arquivo.readlines()]  
 alvo = "bancocn.com"
 
 for subdominio in subdominios:
-	try:
-		sub_alvo = subdominio + "." + alvo 
-		resultado = res.resolve(sub_alvo, "A")
-		for ip in resultado:
-			print(sub_alvo, "->", ip)
-	except:
-		pass
+    try:
+        sub_alvo = f"{subdominio}.{alvo}"
+        print(sub_alvo)
+        resultado = res.resolve(sub_alvo, "A")
+        for ip in resultado:
+            resultado_texto = f"{sub_alvo} -> {ip}\n"
+            print(resultado_texto)
+           
+            with open("results.txt", "a") as arquivo_resultado:
+                arquivo_resultado.write(resultado_texto)
+    except:
+        pass
